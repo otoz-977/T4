927d78c61666179d6c039ca9053d8b2e84fde914
otoniel baeza <otoniel.baeza@gmail.com>
2019-05-16 12:52:57 -0600

    no logre convertir

--- T4/Program.cs
+++ T4/Program.cs
@@ -10,6 +10,52 @@ namespace T4
     {
         static void Main(string[] args)
         {
+            int[] arr = { 1, 8, 4, 7, 9, 5 };
+
+            foreach(int num in arr)
+            {
+                Console.Write(num + " ");
+            }
+            Console.WriteLine("\n");
+            SelectionSort.selectionSort<int>(arr);
+            foreach (int num in arr)
+            {
+                Console.Write(num +" ");
+            }
+            Console.WriteLine("\n");
+
+
+
+        }
+
+
+    }
+    class SelectionSort
+    {
+
+
+        public static void selectionSort<T>(T[] array)
+        {
+            for ( int i = 0; i < array.Length; i++)
+            {
+                T min = array[i];               
+                int  minId = i;
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    
+                    
+                    if (array[j] < min)
+                    {
+                        min = array[j];
+                        minId = j;
+                    }
+                    
+                }
+                // swapping
+                T temp = array[i];
+                array[i] = min;
+                array[minId] = temp;
+            }
         }
     }
 }
